09e32a4f1a8fdc95e6377a5eb6d563745133aac3
Waldo <127903513+Waldo666@users.noreply.github.com>
2024-02-27 23:47:54 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const socket = require("socket.io");
+const PUERTO = 8080;
+require("./database.js");
+
+const productsRouter = require("./routes/products.router.js");
+const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./src/public"));
+
+//Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+//Rutas: 
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
+
+const httpServer = app.listen(PUERTO, () => {
+    console.log(`Srv escuchando en el puerto ${PUERTO}`);
+});
+
